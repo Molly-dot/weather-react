3c5e1e6d9081a3dcfb63029ec5901ffd686a59d0
Molly-dot <76709299+Molly-dot@users.noreply.github.com>
2021-04-02 20:40:58 -0700

    Added axios and react loader spinner

---
+++ src/Weather.js
@@ -0,0 +1,83 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <div className="container">
+        <div className="weather-app">
+          <form className="search-form" id="search-form">
+            <div className="row">
+              <div className="col-6">
+                <input
+                  type="search"
+                  placeholder="Type a city.."
+                  autofocus="on"
+                  autocomplete="off"
+                  id="city-input"
+                  className="form-control shadow-sm"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="form-control btn btn-primary shadow-sm w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <h1 id="city"></h1>
+          <ul>
+            <img
+              src="http://openweathermap.org/img/wn/04d@2x.png"
+              alt="Clear"
+              id="icon"
+              className="float-left"
+            />
+            <li id="date"></li>
+            <li id="description"></li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix">
+                <div className="float-left">
+                  <span className="temperature" id="temperature"></span>
+                  <span className="units">
+                    <a href="#" id="celsius-link" class="active">
+                      °C
+                    </a>{" "}
+                    |
+                    <a href="#" id="fahrenheit-link">
+                      °F
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>
+                  Humidity: <span id="humidity"></span>%
+                </li>
+                <li>
+                  Wind: <span id="wind"></span> km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="row weather-forecast" id="forecast"></div>
+        </div>
+        <a
+          href="https://github.com/Molly-dot/weather-app-updated"
+          target="_blank"
+          style={{ textDecoration: "none" }}
+        >
+          Open-source code
+        </a>
+        by Molly Townsend
+      </div>
+    </div>
+  );
+}
