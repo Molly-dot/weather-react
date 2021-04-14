a0d2cbfbd0fece8b32cff710cd6543515e15a9ae
Molly-dot <76709299+Molly-dot@users.noreply.github.com>
2021-04-14 09:36:14 -0700

    made corrections to footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
 import Weather from "./Weather";
 import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <Weather defaultCity="Paris" />
-
         <footer>
-    
-           <a
-          href="https://github.com/Molly-dot/weather-app-updated"
-          target="_blank"
-          rel="noreferrer"
-          style={{ textDecoration: "none" }}
-        >
-          Open-source code
-        </a>
-        by Molly Townsend
+          <a
+            href="https://github.com/Molly-dot/weather-app-updated"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            Open-source code
+          </a>
+          by Molly Townsend
+        </footer>
       </div>
     </div>
   );
